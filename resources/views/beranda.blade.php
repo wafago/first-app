17c618d32cf4d6ee4f3b2649a2aeb2356a8ee148
wafa <mohammadalyalwafa@gmail.com>
2024-05-10 14:23:38 +0700

    pertama

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sisfo Cup</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div style="width: 100%; height: 100%; padding: 10px; background: white; flex-direction: column; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+    <div style="align-self: stretch; height: 90px; padding-left: 27px; padding-right: 37px; background: #00254A; flex-direction: column; justify-content: flex-start; align-items: center; display: flex">
+        <div style="height: 80px; padding-left: 12px; padding-right: 12px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+            <div style="width: 1296px; height: 80px; position: relative">
+                <div style="left: 580.97px; top: 0px; position: absolute; justify-content: center; align-items: center; display: inline-flex">
+                    <div style="justify-content: flex-start; align-items: center; display: flex">
+                        <div style="padding-left: 8px; padding-right: 8px; padding-top: 29px; padding-bottom: 29px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                            <div style="align-self: stretch; height: 22px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                <div style="color: #FF9900; font-size: 15px; font-family: Inter; font-weight: 400; text-transform: uppercase; line-height: 22px; word-wrap: break-word">Home</div>
+                            </div>
+                        </div>
+                        <div style="padding-left: 2px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                            <div style="padding-left: 8px; padding-right: 8px; padding-top: 29px; padding-bottom: 29px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                <div style="align-self: stretch; height: 22px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                    <div style="color: white; font-size: 14.65px; font-family: Inter; font-weight: 400; text-transform: uppercase; line-height: 22px; word-wrap: break-word">Tim</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="padding-left: 2px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                            <div style="padding-left: 8px; padding-right: 8px; padding-top: 29px; padding-bottom: 29px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                <div style="align-self: stretch; height: 22px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                    <div style="color: white; font-size: 15px; font-family: Inter; font-weight: 400; text-transform: uppercase; line-height: 22px; word-wrap: break-word">Jadwal</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="padding-left: 2px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                            <div style="padding-left: 8px; padding-right: 8px; padding-top: 29px; padding-bottom: 29px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                <div style="align-self: stretch; height: 22px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                    <div style="color: white; font-size: 14.41px; font-family: Inter; font-weight: 400; text-transform: uppercase; line-height: 22px; word-wrap: break-word">Tiket</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="padding-left: 2px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                            <div style="padding-top: 28.50px; padding-bottom: 29px; padding-left: 8px; padding-right: 8px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+                                <div style="align-self: stretch; justify-content: flex-start; align-items: center; gap: 8.50px; display: inline-flex">
+                                    <div style="color: white; font-size: 15px; font-family: Inter; font-weight: 400; text-transform: uppercase; line-height: 22px; word-wrap: break-word">Tentang Kita</div>
+                                    <div style="justify-content: flex-start; align-items: flex-start; display: flex">
+                                        <div style="width: 9px; height: 14px; position: relative">
+                                            <div style="width: 7.79px; height: 4.55px; left: 0.60px; top: 4.51px; position: absolute; background: white"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style="height: 72px; padding-right: 487px; left: 0px; top: 4px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                    <div style="justify-content: flex-start; align-items: center; display: inline-flex">
+                        <div style="padding-left: 20px; padding-right: 28.87px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                            <div style="color: white; font-size: 48px; font-family: Heathergreen; font-weight: 400; text-transform: uppercase; line-height: 72px; letter-spacing: 4.32px; word-wrap: break-word">SISFO CUP 2024</div>
+                        </div>
+                    </div>
+                </div>
+                <div style="left: 1223.86px; top: 18px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                    <div style="align-self: stretch; justify-content: center; align-items: center; display: inline-flex">
+                        <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                            <div style="width: 57.14px; height: 44px; padding-top: 12px; padding-bottom: 12px; padding-left: 12px; padding-right: 11.80px; justify-content: center; align-items: center; gap: 8px; display: inline-flex">
+                                <img style="width: 12px; height: 12px; position: relative; background: linear-gradient(0deg, #555555 0%, #555555 100%); border-radius: 20px; border: 1px white solid" src="https://via.placeholder.com/12x12" />
+                                <div style="width: 13.34px; height: 20px; color: white; font-size: 12px; font-family: Inter; font-weight: 700; text-transform: uppercase; line-height: 20px; word-wrap: break-word">ID</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="align-self: stretch; height: 818px; flex-direction: column; justify-content: center; align-items: center; display: flex">
+        <div style="align-self: stretch; justify-content: center; align-items: center; display: inline-flex">
+            <img style="width: 788px; height: 818px; position: relative" src="https://via.placeholder.com/788x818" />
+        </div>
+        <div style="height: 24px; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: flex">
+            <div style="color: #555555; font-size: 14.12px; font-family: Inter; font-weight: 400; line-height: 24px; word-wrap: break-word">slide 1 of 1</div>
+        </div>
+    </div>
+    <div style="align-self: stretch; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; color: #00254A; font-size: 96px; font-family: Heathergreen; font-weight: 400; text-transform: uppercase; line-height: 24px; letter-spacing: 5.76px; word-wrap: break-word">informasi pendaftaran</div>
+    </div>
+    <div style="align-self: stretch; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; color: #00254A; font-size: 96px; font-family: Heathergreen; font-weight: 400; line-height: 24px; letter-spacing: 5.76px; word-wrap: break-word">TEAM OF SISFO CUP 2024</div>
+    </div>
+    <div style="align-self: stretch; height: 78px; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; text-align: center; color: #00254A; font-size: 48px; font-family: Roboto; font-weight: 600; line-height: 24px; word-wrap: break-word">FAKULTAS</div>
+    </div>
+    <div style="align-self: stretch; padding-left: 464px; padding-right: 464px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+    </div>
+    <div style="align-self: stretch; height: 52px; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; opacity: 0.50; text-align: center; color: #00254A; font-size: 36px; font-family: Roboto; font-weight: 500; line-height: 24px; word-wrap: break-word">show more</div>
+    </div>
+    <div style="align-self: stretch; height: 113px; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; text-align: center; color: #00254A; font-size: 48px; font-family: Roboto; font-weight: 600; line-height: 24px; word-wrap: break-word">SMA/SEDERAJAT</div>
+    </div>
+    <div style="align-self: stretch; padding-left: 464px; padding-right: 464px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+    </div>
+    <div style="align-self: stretch; height: 52px; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; opacity: 0.50; text-align: center; color: #00254A; font-size: 36px; font-family: Roboto; font-weight: 500; line-height: 24px; word-wrap: break-word">show more</div>
+    </div>
+    <div style="align-self: stretch; height: 78px; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; text-align: center; color: #00254A; font-size: 48px; font-family: Roboto; font-weight: 600; line-height: 24px; word-wrap: break-word">SMP/SEDERAJAT</div>
+    </div>
+    <div style="align-self: stretch; padding-left: 464px; padding-right: 464px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+        <div style="width: 100px; height: 100px; background: #D9D9D9"></div>
+    </div>
+    <div style="align-self: stretch; height: 52px; padding-left: 670px; padding-right: 670px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 583px; height: 109px; opacity: 0.50; text-align: center; color: #00254A; font-size: 36px; font-family: Roboto; font-weight: 500; line-height: 24px; word-wrap: break-word">show more</div>
+    </div>
+    <div style="align-self: stretch; padding-left: 745px; padding-right: 745px; padding-top: 33px; padding-bottom: 33px; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+        <div style="width: 440px; height: 109px; text-align: center; color: #00254A; font-size: 96px; font-family: Heathergreen; font-weight: 400; line-height: 24px; letter-spacing: 10.56px; word-wrap: break-word">SPONSOR</div>
+    </div>
+    <div style="align-self: stretch; padding-left: 745px; padding-right: 745px; padding-top: 33px; padding-bottom: 33px; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+        <div style="width: 176px; height: 133px; background: #D9D9D9"></div>
+    </div>
+</div>
+</body>
+
+</html>
